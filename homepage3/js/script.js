e5f232c5a26c1c0370b73dc1ca9516e2de93cb8c
FRONTEND <FRONTEND@MacBook-Pro-Magdalena-Makuch.local>
2023-11-16 14:26:05 +0100

    Initial commit

---
+++ homepage3/js/script.js
@@ -0,0 +1,12 @@
+console.log("Cześć!");
+
+let header = document.querySelector(".header");
+let button = document.querySelector(".header__button");
+let text = document.querySelector(".text");
+
+
+button.addEventListener("click", () => {
+    header.classList.toggle("dark");
+
+    text.innerText = header.classList.contains("dark") ? "Wyłącz" : "Włącz";
+});
