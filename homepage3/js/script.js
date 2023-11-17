cd0ac0fd96ac507c575f5816dccd5773cd9d630c
Magdalena Makuch <makuchmagda02@wp.pl>
2023-11-17 16:55:47 +0100

    add js

--- homepage3/js/script.js
+++ homepage3/js/script.js
@@ -1,7 +1,7 @@
 console.log("Cześć!");
 
 let header = document.querySelector(".header");
-let button = document.querySelector(".header__button");
+let button = document.querySelector(".button");
 let text = document.querySelector(".text");
 
 
